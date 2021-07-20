204957b316803d0b5e5cc28c5cab595cb263e0df
Jonatha Gomes <jonatha.dev.js@gmail.com>
2021-07-20 10:55:44 -0300

    fix: add password column and admin default value

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,13 +1,16 @@
-import { UsersRepositories } from "../repositories/UsersRepositories";
 import { getCustomRepository } from "typeorm";
+import { hash } from "bcryptjs";
+import { UsersRepositories } from "../repositories/UsersRepositories";
+
 type UserRequest = {
   name: string;
   email: string;
-  admin: boolean;
+  admin?: boolean;
+  password: string;
 };
 
 class CreateUserService {
-  async execute({ name, admin, email }: UserRequest) {
+  async execute({ name, admin = false, email, password }: UserRequest) {
     const usersRepository = getCustomRepository(UsersRepositories);
 
     if (!email) {
@@ -20,10 +23,13 @@ class CreateUserService {
       throw new Error("User Already Exists");
     }
 
+    const passwordHash = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
       admin,
+      password: passwordHash,
     });
 
     await usersRepository.save(user);
